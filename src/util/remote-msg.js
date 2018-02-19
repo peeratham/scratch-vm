0b940cb5fee4b2672b133036beab747842c00e90
Peeratham Techapalokul <tpeera4@vt.edu>
2018-02-18 21:10:08 -0500

    add send refactoring event

--- src/util/remote-msg.js
+++ src/util/remote-msg.js
@@ -1,24 +1,31 @@
 const SockJS = require('./sockjs.min.js');
 const Stomp = require('./stomp.min.js').Stomp;
 
-const refactoring_engine_endpoint = "ws://localhost:8080/Q4BlocksWebService/websocketendpoint";
-
 class RemoteMsg {
 	constructor() {
-		this.socket = new SockJS('http://localhost:8080/gs-guide-websocket');
+		this.socket = new SockJS('http://localhost:8080/service-endpoint');
 		this.stompClient = Stomp.over(this.socket);
 		this.stompClient.debug = null;
 		this.stompClient.connect({}, function (frame) {
         	console.log('Connected to the refactoring endpoint');
      		
-        	this.subscribe('/user/queue/hello', function (greeting) {
+        	this.subscribe('/user/queue/request', function (greeting) {
         		console.log(JSON.parse(greeting.body).content);
         	});
     	});		
 	}
 
-	sendEvent(e) {
-		this.stompClient.send("/app/hello", {}, JSON.stringify({'name': 'Karn'}));
+	sendEvent(event) {
+		let message = {};
+		message['type'] = 'invocation';
+		message['body'] = 'body data'
+		message['invocation'] = {
+        	'blocks': event['blocks']["xml"],
+        	'blockId' : event['blockId'],
+        	'refactoring' : event['type']
+        };
+
+		this.stompClient.send("/app/request", {}, JSON.stringify(message));
 	}
 
 }
