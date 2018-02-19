13981008d9e6f609e7bd4c557381e4158198ff7d
Peeratham Techapalokul <tpeera4@vt.edu>
2018-02-19 15:37:09 -0500

    refactor rename arg

--- src/util/remote-msg.js
+++ src/util/remote-msg.js
@@ -2,15 +2,14 @@ const SockJS = require('./sockjs.min.js');
 const Stomp = require('./stomp.min.js').Stomp;
 
 class RemoteMsg {
-	constructor() {
 		this.socket = new SockJS('http://localhost:8080/service-endpoint');
 		this.stompClient = Stomp.over(this.socket);
 		this.stompClient.debug = null;
 		this.stompClient.connect({}, function (frame) {
         	console.log('Connected to the refactoring endpoint');
      		
-        	this.subscribe('/user/queue/request', function (greeting) {
-        		console.log(JSON.parse(greeting.body).content);
+        	this.subscribe('/user/queue/request', function (serverMsg) {
+        		console.log(JSON.parse(serverMsg.body).type);
         	});
     	});		
 	}
