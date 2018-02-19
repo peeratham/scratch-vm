69fa0ba82b2f9832ac202f185a9d1fc0d28a2d9f
Peeratham Techapalokul <tpeera4@vt.edu>
2018-02-19 16:40:32 -0500

    refactor remote message class

--- src/util/remote-msg.js
+++ src/util/remote-msg.js
@@ -5,19 +5,21 @@ class RemoteMsg {
 		this.socket = new SockJS('http://localhost:8080/service-endpoint');
 		this.stompClient = Stomp.over(this.socket);
 		this.stompClient.debug = null;
+  		
   		this.stompClient.connect({}, function (frame) {
-  				this.receiveEvent(frame);
-  			}.bind(this)
-  		);
+  			// connection established
+	  		console.log('Connected to the refactoring endpoint');
+  			
+  			// subscribe upon connection
+  			this.stompClient.subscribe('/user/queue/request', function (serverMsg) {
+  				this.receiveMessage(serverMsg);
+  			}.bind(this));
+
+  		}.bind(this));
 	}
 
-	receiveEvent(frame) {
-		console.log('Connected to the refactoring endpoint');
-		
-		this.stompClient.subscribe('/user/queue/request', function (serverMsg) {
-			console.log(JSON.parse(serverMsg.body).type);
-		});
-
+	receiveMessage(serverMsg) {
+		console.log(JSON.parse(serverMsg.body).type);
 	}
 
 	sendEvent(event) {
